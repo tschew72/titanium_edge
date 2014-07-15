e8a32d32375eb4254ff0e1e0d0797d62340fd0da
Vince <tschew@ViiVaPC.local>
2014-07-15 14:55:56 +0800

    working version- simple login with no password. allow possing of parameters from auth/login to /edge

--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,5 @@
 bundle install
 git init
 git add .
-git commit -m "working version with bootstrap"
+git commit -m "working version- simple login with no password. allow possing of parameters from auth/login to /edge"
 git push heroku master
