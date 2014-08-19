31864a6d691259eebd496f4e8a43f9159be3ae1d
Vince <tschew@ViiVaPC.local>
2014-08-19 14:22:49 +0800

    Version without edit/create/delete functionality

--- public/vendor/custom.js
+++ public/vendor/custom.js
@@ -2,6 +2,7 @@
    $(document).ready(function() {
       
       $('#firstname').editable();
+      $('#lastname').editable();
       $('#age').editable();
       $('#gender').editable({   
         source: [
