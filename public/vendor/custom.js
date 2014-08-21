267c89ada138198887f4e5ebcd44a5a947ea93ef
Vince <tschew@ViiVaPC.local>
2014-08-21 10:28:36 +0800

    Version without edit/create/delete functionality

--- public/vendor/custom.js
+++ public/vendor/custom.js
@@ -63,46 +63,9 @@
     });
 
 
-
-             $('.easy-pie-chart.percentage').each(function(){
-                    var $box = $(this).closest('.infobox');
-                    var barColor = $(this).data('color') || (!$box.hasClass('infobox-dark') ? $box.css('color') : 'rgba(255,255,255,0.95)');
-                    var trackColor = barColor == 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)' : '#E2E2E2';
-                    var size = parseInt($(this).data('size')) || 50;
-                    $(this).easyPieChart({
-                        barColor: barColor,
-                        trackColor: trackColor,
-                        scaleColor: false,
-                        lineCap: 'butt',
-                        lineWidth: parseInt(size/10),
-                        animate: /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase()) ? false : 1000,
-                        size: size
-                    });
-                })
-            
-        $('.sparkline').each(function(){
-          var $box = $(this).closest('.infobox');
-          var barColor = !$box.hasClass('infobox-dark') ? $box.css('color') : '#FFF';
-          $(this).sparkline('html', {tagValuesAttribute:'data-values', type: 'bar', barColor: barColor , chartRangeMin:$(this).data('min') || 0} );
-        });
-
-
       
       
-        $('#recent-box [data-rel="tooltip"]').tooltip({placement: tooltip_placement});
-        function tooltip_placement(context, source) {
-          var $source = $(source);
-          var $parent = $source.closest('.tab-content')
-          var off1 = $parent.offset();
-          var w1 = $parent.width();
-      
-          var off2 = $source.offset();
-          var w2 = $source.width();
-      
-          if( parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2) ) return 'right';
-          return 'left';
-        }
-      
+     
       
         $('.dialogs,.comments').slimScroll({
           height: '300px'
