dfdb71bee9c2e3a28c337b5f995b0ae363cffe37
Vince <tschew@ViiVaPC.local>
2014-08-26 23:27:01 +0800

    Add Skill functionality established.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,5 +5,5 @@
 #   and it will cause strange issues if you don't re-establish all connections correctly
 # * Heroku has a 30s timeout so we set to 60, allowing them to handle timeouts and issue an H12 error
 # * Depending on your memory usage workers can be bumped to 3-4. With Sinatra it can be 8+
-worker_processes 3
+worker_processes 8
 timeout 60
