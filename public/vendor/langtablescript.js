76ff87ac3c6944ad3b6d31b419b3adac2a65d492
Vince <tschew@gmail.com>
2014-09-22 14:25:56 +0800

    The new Dashboard and Frontpage

--- public/vendor/langtablescript.js
+++ public/vendor/langtablescript.js
@@ -1,11 +1,11 @@
-        $('#languagesettingtable').dataTable( {   //need to include the Ajax source so that I can reload it from anywhere
+        $('#languagesettingtable').dataTable( {
             "scrollY": 200,
             "ScrollX": true,
             "iDisplayLength": 10,
             "order": [[ 0, "desc" ]],
             "scrollCollapse": true,
             "jQueryUI": true,
-            "aoColumns": [         
+            "aoColumns": [        
                 null, null, null,
                 { "bSortable": false }
               ]
