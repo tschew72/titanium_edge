cd89679638a6746a706f1192deb59b5bea6282f1
Vince <tschew@gmail.com>
2014-10-03 16:25:39 +0800

    The new Dashboard and Frontpage

---
+++ public/vendor/jobdetailscript.js
@@ -0,0 +1,8 @@
+
+     $("#revealcheck").on('change', function(){
+          $.ajax({
+            url: '/updaterevealcoy',
+            type: 'POST',
+            data: {"job_companyreveal": this.checked, "pk" : <%=@joblisting.id %>}
+          });
+      });
