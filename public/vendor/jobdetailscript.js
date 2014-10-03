255f22dd1666b8c96100cd9f3ffc86ecd922d3d1
Vince <tschew@gmail.com>
2014-10-03 17:04:17 +0800

    The new Dashboard and Frontpage

--- public/vendor/jobdetailscript.js
+++ public/vendor/jobdetailscript.js
@@ -5,3 +5,9 @@
     //   emptytext: '--',
     // // placeholder: '[Optional] Enter your achievement here'
     // });
+
+  $('#job_contactname').editable(
+    {
+      emptytext: '--',
+    // placeholder: '[Optional] Enter your achievement here'
+  });
