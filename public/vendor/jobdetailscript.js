9fd9655644bb451d53bace98ca73879f4fac6381
Vince <tschew@gmail.com>
2014-10-03 16:50:24 +0800

    The new Dashboard and Frontpage

--- public/vendor/jobdetailscript.js
+++ public/vendor/jobdetailscript.js
@@ -8,8 +8,8 @@
       });
 
 
-       $('#job_contactname').editable(
-    {
-      emptytext: '--',
-    // placeholder: '[Optional] Enter your achievement here'
-  });
+    // $('#job_contactname').editable(
+    // {
+    //   emptytext: '--',
+    // // placeholder: '[Optional] Enter your achievement here'
+    // });
